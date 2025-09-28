9561b0252cb18de9c186d6ba84c8f957939fba5a
desrig869 <des@talos-admin.tazor.lan>
2025-09-28 14:58:07 +0100

    calico eBPF config

--- Talos/calico/calico-ebpf.sh
+++ Talos/calico/calico-ebpf.sh
@@ -1,8 +1,16 @@
-kubectl apply -f  calico-config-map.yaml
-
+# kubectl apply -f  calico-config-map.yaml
+kubectl apply -f -<<EOF
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: kubernetes-services-endpoint
+  namespace: tigera-operator
+data:
+  KUBERNETES_SERVICE_HOST: '192.168.130.60'
+  KUBERNETES_SERVICE_PORT: '6443'
+EOF
 
 kubectl patch installation.operator.tigera.io default --type merge -p '{"spec":{"calicoNetwork":{"linuxDataplane":"BPF"}}}'
-
 
 kubectl patch ds -n kube-system kube-proxy -p '{"spec":{"template":{"spec":{"nodeSelector":{"non-calico": "true"}}}}}'
 
